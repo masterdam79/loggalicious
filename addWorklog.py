ef759e07972b47edbc397a1e70f28125608172ec
Richard Reijmers <r.reijmers@global.leaseweb.com>
2018-06-28 13:57:18 +0200

    Added all parameters, need to format datetime on icsparser though

--- addWorklog.py
+++ addWorklog.py
@@ -5,6 +5,8 @@ import configparser
 from jira import JIRA
 from jira.resources import Worklog
 import sys
+from datetime import datetime
+from tzlocal import get_localzone
 
 # Get some variables outside this script
 config = configparser.ConfigParser()
@@ -12,6 +14,8 @@ config.read('./config.txt')
 jira_user = config['BASICAUTH']['JIRA_USER']
 jira_pass = config['BASICAUTH']['JIRA_PASS']
 jira_url = config['BASICAUTH']['JIRA_URL']
+
+tz = get_localzone()
 
 jira = JIRA(basic_auth=(jira_user, jira_pass), options = {'server': jira_url})
 
@@ -27,10 +31,16 @@ parser = MyParser()
 parser.add_argument('-i', '--jira_item', type=str, metavar='JIRA Key', required=True, help="The JIRA key you want to log hours on, like ABC-123")
 
 # :param started: Moment when the work is logged, based on REST Browser it needs: "2014-06-03T08:21:01.273+0000"
-parser.add_argument('-d', '--date', type=str, metavar='Date', required=True, help="Format: 2014-06-03T08:21:01.273+0000")
+parser.add_argument('-d', '--date', type=str, metavar='Date', required=True, help="Format: 2018-06-28 18:00")
 parser.add_argument('-n', '--description', type=str, metavar='Description', required=True, help="Fill in what you have done")
 parser.add_argument('-w', '--worked', type=str, metavar='Time-Worked', required=True, help="Time worked, format: 2h, 1d")
 
 args = parser.parse_args()
 
-jira.add_worklog(args.jira_item, timeSpent=args.worked)
+datetime = datetime.strptime(args.date, '%Y-%m-%d %H:%M')
+datetime = tz.localize(datetime)
+
+
+#exit(0)
+# https://jira.readthedocs.io/en/master/api.html#jira.JIRA.add_worklog
+jira.add_worklog(args.jira_item, timeSpent=args.worked, started=datetime, comment=args.description)
