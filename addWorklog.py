9e1a364afcbbc5bc2fe666ac5631adb9b75e4e3b
Richard Reijmers <r.reijmers@global.leaseweb.com>
2018-06-28 10:15:16 +0200

    Show help on not filling in required arguments

--- addWorklog.py
+++ addWorklog.py
@@ -4,6 +4,7 @@ import argparse
 import configparser
 from jira import JIRA
 from jira.resources import Worklog
+import sys
 
 # Get some variables outside this script
 config = configparser.ConfigParser()
@@ -14,28 +15,23 @@ jira_url = config['BASICAUTH']['JIRA_URL']
 
 jira = JIRA(basic_auth=(jira_user, jira_pass), options = {'server': jira_url})
 
-parser = argparse.ArgumentParser(description='Process some arguments.')
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+parser = MyParser()
 
 # A JIRA item
-parser.add_argument('--jira_item', type=str)
+parser.add_argument('-i', '--jira_item', type=str, metavar='JIRA Key', required=True, help="The JIRA key you want to log hours on, like ABC-123")
 
 # :param started: Moment when the work is logged, based on REST Browser it needs: "2014-06-03T08:21:01.273+0000"
-parser.add_argument('--date', type=str)
-parser.add_argument('--description', type=str)
-parser.add_argument('--worked', type=str)
+parser.add_argument('-d', '--date', type=str, metavar='Date', required=True, help="Format: 2014-06-03T08:21:01.273+0000")
+parser.add_argument('-n', '--description', type=str, metavar='Description', required=True, help="Fill in what you have done")
+parser.add_argument('-w', '--worked', type=str, metavar='Time-Worked', required=True, help="Time worked, format: 2h, 1d")
 
 args = parser.parse_args()
 
-for arg in vars(args):
-    argvalue = getattr(args, arg)
-    if str(argvalue) == 'None':
-        print "No argument given for " + str(arg)
-        exec(arg + " = raw_input('Give value for ' + arg + ': ')")
-        #print eval(arg)
-        if eval(arg) == "":
-            print "No value given, exiting, try again.."
-            sys.exit(0)
-    else:
-        exec(arg + " = argvalue")
 
-jira.add_worklog(jira_item, timeSpent=worked)
+jira.add_worklog(jira_item, timeSpent='2h')
