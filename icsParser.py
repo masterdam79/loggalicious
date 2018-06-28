4ebafc2ffe4a089d504f36b259f7a1815a76e379
Richard Reijmers <r.reijmers@global.leaseweb.com>
2018-06-28 19:59:38 +0200

    Redid my lost commits

--- icsParser.py
+++ icsParser.py
@@ -75,19 +75,32 @@ for component in gcal.walk():
                 print(date)
 
                 # Define regex to match JIRA keys
-                regex = r"[A-Z]*-[0-9]*"
+                regex = r"[A-Z]+-[0-9]+"
 #                regexp = re.compile(r'[A-Z]*\-[0-9]*', re.MULTILINE)
 #                if (re.search(regex, summary) or re.search(regex, description)):
-                if (re.search(regex, summary) or re.search(regex, description)):
-                    print('found a match!')
+                if re.search(regex, summary):
+                    print('Found a match in summary!')
 #                if (re.search(regexp, summary) or re.search(regexp, description)):
                     jira_key_summary = re.search(regex, summary).group()
 #                    jira_key_description = re.search(regex, description).group()
-                    print '======matched======'
+                    print('======matched======')
                     print("KEYS: " + jira_key_summary)
 #                    print("KEYS: " + jira_key_summary + " - " + jira_key_description)
-                    print '======matched======'
+                    print('======matched======')
 #                    python addWorklog.py --jira_item jira_key_summary --date date --worked duration.total_seconds() + "s" --description description
+                elif re.search(regex, description):
+                    print('Found a match in description!')
+#                if (re.search(regexp, summary) or re.search(regexp, description)):
+                    jira_key_description = re.search(regex, description).group()
+#                    jira_key_description = re.search(regex, description).group()
+                    print('======matched======')
+                    print("KEYS: " + jira_key_description)
+#                    print("KEYS: " + jira_key_summary + " - " + jira_key_description)
+                    print('======matched======')
+#                    python addWorklog.py --jira_item jira_key_summary --date date --worked duration.total_seconds() + "s" --description description
+                else:
+                    print('Non here')
+
 
                 print('-------------------------')
 
